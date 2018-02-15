7ebd46cea576d19fe8041efadc25c1971f904929
Vinicius Santos <vinicius.santos@jointecnologia.com.br>
2018-02-15 12:30:56 -0200

    Configuration initial for mongodb

---
+++ backend/src/config/data.js
@@ -0,0 +1,16 @@
+let data = {
+    user: [
+        {
+            "username": "manager",
+            "password": "Boss123",
+            "user_type": "Manager"
+        },
+        {
+            "username": "employee",
+            "password": "Employee123",
+            "user_type": "Employee"
+        }
+    ]
+}
+
+module.exports = data
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,2 +1,14 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://mongodb-poatek:27017/poatek');
+const data = require('./data')
+const mongoose = require('mongoose')
+const User = require('../models/user')
+mongoose.connect('mongodb://mongodb-poatek:27017/poatek')
+
+// Insert users default
+User.find((error, users) => {
+    if (users.length <= 0) {
+        User.create(data.user, function (err, transaction) {
+            if (err)
+                console.log({ error: error })
+        })
+    }
+})
